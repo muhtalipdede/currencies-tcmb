3b189cf8081be223e7ba806ceea791e45e7f3ab3
Muhtalip Dede <muhtalipdede@gmail.com>
2024-12-07 00:47:16 +0300

    Refactor currency rate retrieval and storage

--- Readme.md
+++ Readme.md
@@ -1,6 +1,21 @@
 # Currencies with TCMB
+This service provides the latest currency rates from the Central Bank of the Republic of Turkey (TCMB).
 
-![Image](/usd_try.png)
+## Description
+This service provides the latest currency rates from the Central Bank of the Republic of Turkey (TCMB).
+
+## Images
+<!-- ["TP.DK.USD.S.YTL", "TP.DK.USD.A.YTL", "TP.DK.EUR.S.YTL", "TP.DK.EUR.A.YTL", "TP.DK.GBP.S.YTL", "TP.DK.GBP.A.YTL", "TP.DK.CHF.S.YTL", "TP.DK.CHF.A.YTL", "TP.DK.JPY.S.YTL", "TP.DK.JPY.A.YTL"] -->
+![TP.DK.USD.S.YTL](currency/TP_DK_USD_S_YTL.png)
+![TP.DK.USD.A.YTL](currency/TP_DK_USD_A_YTL.png)
+![TP.DK.EUR.S.YTL](currency/TP_DK_EUR_S_YTL.png)
+![TP.DK.EUR.A.YTL](currency/TP_DK_EUR_A_YTL.png)
+![TP.DK.GBP.S.YTL](currency/TP_DK_GBP_S_YTL.png)
+![TP.DK.GBP.A.YTL](currency/TP_DK_GBP_A_YTL.png)
+![TP.DK.CHF.S.YTL](currency/TP_DK_CHF_S_YTL.png)
+![TP.DK.CHF.A.YTL](currency/TP_DK_CHF_A_YTL.png)
+![TP.DK.JPY.S.YTL](currency/TP_DK_JPY_S_YTL.png)
+![TP.DK.JPY.A.YTL](currency/TP_DK_JPY_A_YTL.png)
 
 ## Create a virtual environment
 ```bash
--- __main__.py
+++ __main__.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 
 load_dotenv()
 
-def get_exchange_rates(start_date, end_date):
+def get_exchange_rates(series, start_date, end_date):
     try:
         api_key = os.environ.get("TCMB_API_KEY")
         if api_key is None:
             raise ValueError("TCMB_API_KEY ortam değişkeni bulunamadı.")
         
-        url = f"https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.S.YTL&startDate={start_date}&endDate={end_date}&type=json"
+        url = f"https://evds2.tcmb.gov.tr/service/evds/series={series}&startDate={start_date}&endDate={end_date}&type=json"
         print(url)
         headers = {
             "key": api_key,
@@ -30,13 +30,16 @@ def get_exchange_rates(start_date, end_date):
         print(f"Error: {e}")
         return None
     
-def write_to_csv(exchange_rate_date, exchange_rate):
+def write_to_csv(exchange_rate_date, exchange_rate, currency_name):
     try:
-        file_path = "usd_try.csv"
+        currency_dir = "currency"
+        if not os.path.exists(currency_dir):
+            os.makedirs(currency_dir)
+        file_path = currency_dir + "/" + currency_name + ".csv"
         if not os.path.exists(file_path):
             data = {
                 "Tarih": [exchange_rate_date],
-                "USD/TRY": [exchange_rate],
+                currency_name: [exchange_rate],
             }
             df = pd.DataFrame(data)
             df.to_csv(file_path, index=False)
@@ -44,7 +47,7 @@ def write_to_csv(exchange_rate_date, exchange_rate):
         try:
             df = pd.read_csv(file_path)
         except pd.errors.EmptyDataError:
-            df = pd.DataFrame(columns=["Tarih", "USD/TRY"])
+            df = pd.DataFrame(columns=["Tarih", currency_name])
         
         if exchange_rate_date in df["Tarih"].values:
             print("Veri zaten mevcut.")
@@ -53,7 +56,7 @@ def write_to_csv(exchange_rate_date, exchange_rate):
         exchange_rate_date = datetime.datetime.strptime(exchange_rate_date, "%d-%m-%Y").strftime("%d-%m-%Y")
         new_data = {
             "Tarih": [exchange_rate_date],
-            "USD/TRY": [exchange_rate],
+            currency_name: [exchange_rate],
         }
         new_df = pd.DataFrame(new_data)
         df = pd.concat([df, new_df], ignore_index=True)
@@ -61,27 +64,40 @@ def write_to_csv(exchange_rate_date, exchange_rate):
     except Exception as e:
         print(f"Error: {e}")
 
+def plot_all_csv_files(file_list):
+    try:
+        for file in file_list:
+            file_path = "currency/" + file
+            df = pd.read_csv(file_path)
+            df["Tarih"] = pd.to_datetime(df["Tarih"], format="%d-%m-%Y")
+            df = df.sort_values(by="Tarih")
+            df.plot(x="Tarih", y=file.replace(".csv", ""), kind="line")
+            image_file_name = file.replace(".csv", ".png")
+            image_file_name = "currency/" + image_file_name
+            plt.savefig(image_file_name)
+            plt.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+series_list = ["TP.DK.USD.S.YTL", "TP.DK.USD.A.YTL", "TP.DK.EUR.S.YTL", "TP.DK.EUR.A.YTL", "TP.DK.GBP.S.YTL", "TP.DK.GBP.A.YTL", "TP.DK.CHF.S.YTL", "TP.DK.CHF.A.YTL", "TP.DK.JPY.S.YTL", "TP.DK.JPY.A.YTL"]
 def main():
     end_date = datetime.date.today().strftime("%d-%m-%Y")
-    start_date = datetime.date.today() - datetime.timedelta(days=30)
+    start_date = datetime.date.today() - datetime.timedelta(days=1000)
     start_date = start_date.strftime("%d-%m-%Y")
-    exchange_rates = get_exchange_rates(start_date, end_date)
-    for exchange_rate_item in exchange_rates:
-        print(exchange_rate_item)
-        exchange_rate = exchange_rate_item["TP_DK_USD_S_YTL"]
-        exchange_rate_date = exchange_rate_item["Tarih"]
-        if exchange_rate is not None and exchange_rate_date is not None:
-            write_to_csv(exchange_rate_date, exchange_rate)
-            print("Veri başarıyla kaydedildi.")
-        else:
-            print("Veri kaydedilemedi.")
+    file_list = []
+    for series in series_list:
+        exchange_rates = get_exchange_rates(series, start_date, end_date)
+        series_name = series.replace(".", "_")
+        file_list.append(series_name + ".csv")
+        for exchange_rate_item in exchange_rates:
+            exchange_rate = exchange_rate_item[series_name]
+            exchange_rate_date = exchange_rate_item["Tarih"]
+            if exchange_rate is not None and exchange_rate_date is not None:
+                write_to_csv(exchange_rate_date, exchange_rate, series_name)
+            else:
+                print("Veri kaydedilemedi.")
 
-    ## plot usd_try.csv and save as usd_try.png
-    df = pd.read_csv("usd_try.csv")
-    df["Tarih"] = pd.to_datetime(df["Tarih"], format="%d-%m-%Y")
-    df = df.sort_values(by="Tarih")
-    df.plot(x="Tarih", y="USD/TRY", kind="line")
-    plt.savefig("usd_try.png")
+    plot_all_csv_files(file_list)
 
 if __name__ == "__main__":
     main()
